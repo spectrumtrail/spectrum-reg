0a77ac9893a639f3c3039e41505a900920d27b2b
ColinW520 <colinhagan@gmail.com>
2018-11-02 11:32:05 -0500

    specify paperclip production defaults

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,24 @@
 Rails.application.configure do
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET_ID'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    },
+    :s3_region => ENV['AWS_REGION']
+  }
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_region: ENV['AWS_REGION'],
+    s3_credentials: {
+      bucket: ENV['S3_BUCKET_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    },
+    s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
